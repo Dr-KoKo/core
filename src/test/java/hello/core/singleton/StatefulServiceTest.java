3f291f497dc049ca861d1b576016131b54ccd98a
Denny-Ko <donghar@naver.com>
2023-04-06 19:51:34 +0900

    싱글톤 컨테이너(2) updated on Apr 6th 2023

---
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,41 @@
+package hello.core.singleton;
+
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void statefulServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean("statefulService", StatefulService.class);
+        StatefulService statefulService2 = ac.getBean("statefulService", StatefulService.class);
+
+        //ThreadA : 사용자A 10000원 주문
+        statefulService1.order("userA", 10000);
+        //ThreadB : 사용자B 20000원 주문
+        statefulService2.order("userA", 20000);
+
+        //ThreadA : 사용자A 주문 금액 조회
+        int price = statefulService1.getPrice();
+        System.out.println("price = " + price);
+
+        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    static class TestConfig{
+
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+}
